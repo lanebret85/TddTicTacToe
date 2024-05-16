163ec034303afea2cfe2f5228fdf2da226c9d626
Lane Bretschneider <lane.bretschneider@ibotta.com>
2024-05-16 16:57:40 -0600

    add switchTokenTo method

--- src/main/kotlin/com/example/demo/Rules.kt
+++ src/main/kotlin/com/example/demo/Rules.kt
@@ -1,4 +1,16 @@
 package org.example.com.example.demo
 
 class Rules {
+    fun switchTokenTo(player: String, token: StringBuilder) {
+        when (player) {
+            "user" -> {
+                token.clear()
+                token.append("X")
+            }
+            "cpu" -> {
+                token.clear()
+                token.append("O")
+            }
+        }
+    }
 }
