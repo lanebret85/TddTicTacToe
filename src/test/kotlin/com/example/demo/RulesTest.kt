3f23c40ed2b00c754ff6dab70c78caccc6bf616b
Lane Bretschneider <lane.bretschneider@ibotta.com>
2024-05-17 14:10:00 -0600

    add tests for switching the token depending on the player argument

--- src/test/kotlin/com/example/demo/RulesTest.kt
+++ src/test/kotlin/com/example/demo/RulesTest.kt
@@ -1,4 +1,29 @@
 package com.example.demo
 
+import org.example.com.example.demo.Rules
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class RulesTest {
+    @Test
+    fun testSwitchTokenToX() {
+        val rules = Rules()
+        val player = "user"
+        val token = StringBuilder()
+
+        rules.switchTokenTo(player, token)
+
+        Assertions.assertTrue(token.toString() == "X")
+    }
+
+    @Test
+    fun testSwitchTokenToO() {
+        val rules = Rules()
+        val player = "cpu"
+        val token = StringBuilder()
+
+        rules.switchTokenTo(player, token)
+
+        Assertions.assertTrue(token.toString() == "O")
+    }
 }
