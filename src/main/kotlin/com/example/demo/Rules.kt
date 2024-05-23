ad691fb064f00eb98bae142a2400807fb7242b00
Lane Bretschneider <lane.bretschneider@ibotta.com>
2024-05-23 16:13:50 -0600

    refactor to change out user and player naming in switchTokenTo method, add isWinner method

--- src/main/kotlin/com/example/demo/Rules.kt
+++ src/main/kotlin/com/example/demo/Rules.kt
@@ -1,9 +1,9 @@
 package org.example.com.example.demo
 
 class Rules {
-    fun switchTokenTo(player: String, token: StringBuilder) {
-        when (player) {
-            "user" -> {
+    fun switchTokenTo(user: String, token: StringBuilder) {
+        when (user) {
+            "player" -> {
                 token.clear()
                 token.append("X")
             }
@@ -16,5 +16,14 @@ class Rules {
 
     fun piecePlacedSuccessfully(input: Int): Boolean {
         return input in 1..9
+    }
+
+    fun isWinner(gameBoard: GameBoard, userConditions: List<Int>): Boolean {
+        for(condition in gameBoard.generateWinConditions()) {
+            if (userConditions.containsAll(condition)) {
+                return true
+            }
+        }
+        return false
     }
 }
